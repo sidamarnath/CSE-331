19b35fb9a7cf51b8b3da9978ec9ec94c2a8852a8
sidamarnath <75329890+sidamarnath@users.noreply.github.com>
2023-02-16 01:11:08 -0500

    Add files via upload

---
+++ CC1/solution.py
@@ -0,0 +1,28 @@
+"""
+CC1 Student Submission
+Name: Sidharth Amarnath
+"""
+
+from typing import List, Tuple
+
+
+def farmer_fencing(points: List[Tuple[int, int]]) -> int:
+    """
+    Calculates the minimum perimeter required to form a rectangle using the
+    given points.
+    :param points: The fence posts available to use.
+    :return: The minimum perimeter of the fence.
+    """
+    result = 0
+    # rectangle = False
+    if len(points) < 4:
+        return 0
+
+
+
+
+
+
+
+
+
